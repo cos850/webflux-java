a6c5dedbec0367e5a1bfa88c9cf8f9b3a35ef860
HyeRiJeong <cos850@naver.com>
2024-10-24 07:05:39 +0900

    Reactor Mono And Flux Example

--- src/main/java/com/hr/webfluxjava/HelloReactor.java
+++ src/main/java/com/hr/webfluxjava/HelloReactor.java
@@ -1,10 +1,35 @@
 package com.hr.webfluxjava;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class HelloReactor {
     public static void main(String[] args) {
-        Mono.just("Hello Reactor!") // publisher 가 데이터를 제공
-                .subscribe(message -> System.out.println(message)); // subscriber 가 데이터 받음
+//        helloMono();
+//        helloFlux();
+
+        // mono sample
+        monoEmitCompleteSignalWithNoData();
     }
+
+    public static void helloMono() {
+        Mono.just("Hello Reactor!") // Mono의 just operator를 사용해 한 개의 데이터를 downStream으로 emit한다 (onNext signal 에 실어서 보낸다)
+                .subscribe(message -> System.out.println(message)); // emit된 데이터는 람다 표현식의 인자로 들어옴.
+    }
+
+    public static void helloFlux() {
+        Flux.just("Hello", "Reactor")
+                .map(message -> message.toLowerCase())
+                .subscribe(message -> System.out.print(message + " "));
+    }
+
+    public static void monoEmitCompleteSignalWithNoData(){
+        Mono.empty()
+            .subscribe(
+                    data -> System.out.println("emitted data = " + data),
+                    error -> System.out.println("emitted onError signal"),
+                    () -> System.out.println("emitted onComplete signal")
+            );
+    }
+
 }
